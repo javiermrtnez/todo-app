83bd8d3db349ec92d53f756e32f4aab4d8cf1142
javiermrtnez <javiermartinezbueno@gmail.com>
2023-04-01 20:49:29 +0200

    fix: renamed status when creating it

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
         {
           id: allTodos.length,
           value: newTodoValue,
-          status: TODO_STATUS.active,
+          status: TODO_STATUS.ACTIVE,
         },
       ]);
 
