cd4c0950f19fdcc09a026015a4c6853c1ff046e3
javiermrtnez <javiermartinezbueno@gmail.com>
2023-04-02 22:52:02 +0200

    REVER - Mocked data and theme for FrontEnd Mentor screenshot

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -16,7 +16,7 @@ export const ThemeContextProvider = ({ children }) => {
       ? THEMES.dark
       : THEMES.light;
 
-  const [theme, setTheme] = useState(THEMES.dark);
+  const [theme, setTheme] = useState(getSystemTheme());
 
   const toggleTheme = () => {
     theme === THEMES.light ? setTheme(THEMES.dark) : setTheme(THEMES.light);
