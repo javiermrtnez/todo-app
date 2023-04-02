154c595cb5e1caf581001034c1ad482d9ff6b37c
javiermrtnez <javiermartinezbueno@gmail.com>
2023-04-02 16:12:35 +0200

    feat: added logic and styles to filter todo items

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './App.css';
 import iconMoon from './assets/icons/icon-moon.svg';
 import iconSun from './assets/icons/icon-sun.svg';
@@ -6,9 +6,35 @@ import iconCross from './assets/icons/icon-cross.svg';
 import iconSend from './assets/icons/icon-send.svg';
 import { ThemeContext, THEMES } from './context/ThemeContext';
 
+const FILTERS = {
+  ALL: 'ALL',
+  ACTIVE: 'ACTIVE',
+  COMPLETE: 'COMPLETE',
+};
+
+const EMPTY_TODO_FILTERS_TEXT = {
+  ALL: 'left',
+  ACTIVE: 'active',
+  COMPLETE: 'completed',
+};
+
 const App: React.FC = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
   const [toDos, setToDos] = useState([]);
+  const [filteredToDos, setFilteredToDos] = useState(toDos);
+  const [filter, setFilter] = useState(FILTERS.ALL);
+
+  useEffect(() => {
+    if (filter === FILTERS.ACTIVE) {
+      setFilteredToDos(toDos.filter((toDo) => toDo.active));
+    } else if (filter === FILTERS.COMPLETE) {
+      setFilteredToDos(toDos.filter((toDo) => !toDo.active));
+    } else {
+      setFilteredToDos(toDos);
+    }
+  }, [toDos, filter]);
+
+  const itemsLeft = toDos.filter((toDo) => toDo.active).length;
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
@@ -27,8 +53,6 @@ const App: React.FC = () => {
       e.target.reset();
     }
   };
-
-  const itemsLeft = toDos.filter((toDo) => toDo.active).length;
 
   const handleCheckboxClick = (idToComplete) => {
     setToDos((prevTodos) => {
@@ -49,10 +73,14 @@ const App: React.FC = () => {
     setToDos((prevTodos) => prevTodos.filter((todo) => todo.id !== idToRemove));
   };
 
+  const handleFilterButton = (e) => {
+    setFilter(e.target.id);
+  };
+
   return (
     <div className='todo-app'>
       <div className='header-container'>
-        <h1>TODO</h1>
+        <h1>TO-DO</h1>
 
         <button className='toggle-theme-button' onClick={toggleTheme}>
           <img src={theme === THEMES.light ? iconMoon : iconSun}></img>
@@ -68,13 +96,14 @@ const App: React.FC = () => {
       </form>
 
       <div className='todos-container'>
-        {toDos.length > 0 ? (
-          toDos.map(({ id, value, active }) => (
+        {filteredToDos.length > 0 ? (
+          filteredToDos.map(({ id, value, active }) => (
             <div className='todo-item' key={id}>
               <div className='checkbox-text-container'>
                 <input
+                  checked={!active}
                   type='checkbox'
-                  onClick={() => {
+                  onChange={() => {
                     handleCheckboxClick(id);
                   }}
                 />
@@ -96,12 +125,46 @@ const App: React.FC = () => {
           ))
         ) : (
           <div className='todo-item'>
-            <span className='todo-empty-text'>No todo items left!</span>
+            <span className='todo-empty-text'>
+              No to-do items {EMPTY_TODO_FILTERS_TEXT[filter]}!
+            </span>
           </div>
         )}
 
         <div className='todo-resume-item'>
           <span>{itemsLeft} items left</span>
+
+          <div className='filter-buttons-container'>
+            <button
+              className={`filter-button${
+                filter === FILTERS.ALL ? ' active' : ''
+              }`}
+              id={FILTERS.ALL}
+              onClick={handleFilterButton}
+            >
+              All
+            </button>
+
+            <button
+              className={`filter-button${
+                filter === FILTERS.ACTIVE ? ' active' : ''
+              }`}
+              id={FILTERS.ACTIVE}
+              onClick={handleFilterButton}
+            >
+              Active
+            </button>
+
+            <button
+              className={`filter-button${
+                filter === FILTERS.COMPLETE ? ' active' : ''
+              }`}
+              id={FILTERS.COMPLETE}
+              onClick={handleFilterButton}
+            >
+              Completed
+            </button>
+          </div>
         </div>
       </div>
     </div>
