f89f8aa3c787c32a7b2190ca4b580355f475e150
javiermrtnez <javiermartinezbueno@gmail.com>
2023-04-12 23:20:00 +0200

    eslint: config consistent type imports off

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,7 @@ module.exports = {
     'react'
   ],
   rules: {
-    "@typescript-eslint/return-await": "off"
+    "@typescript-eslint/return-await": "off",
+    "@typescript-eslint/consistent-type-imports": "off"
   }
 }
